e2e5eb20ac0c18920225de1d23edabb7b300f78c
jcmb <jcmb@users.noreply.github.com>
2026-05-02 10:35:14 -0600

    Add encoding

--- Public/RetTime.py
+++ Public/RetTime.py
@@ -6,7 +6,7 @@ from struct import *
 from binascii import hexlify
 from ENUM import enum
 from DCOL_Decls import *
-
+from pprint import pprint
 
 
 class RetTime (DCOL.Dcol) :
@@ -25,17 +25,17 @@ class RetTime (DCOL.Dcol) :
 
     def decode(self,data,internal=False):
         if len (data) == 0x14:
-            unpacked = unpack('>7s L H 4s 3s' ,str(data))
-            del data[0:calcsize('> 7s L H 4s 3s')]
+            unpacked = unpack('>7s 4s 2s 4s 3s', data)
+            del data[0:calcsize('>7s 4s 2s 4s 3s')]
             self.Subtype =0
-            self.Time=unpacked[0]
-            self.Week_Number=unpacked[1]
-            self.UTC_Offset=unpacked[2]
-            self.Time_Offset=unpacked[3]
-            self.Time_Mode=unpacked[4]
+            self.Time=unpacked[0].decode("ascii")
+            self.Week_Number=unpacked[1].decode("ascii")
+            self.UTC_Offset=unpacked[2].decode("ascii")
+            self.Time_Offset=unpacked[3].decode("ascii")
+            self.Time_Mode=unpacked[4].decode("ascii")
         elif len (data) == 0x4:
             self.Subtype =1;
-            unpacked = unpack('>L' ,str(data))
+            unpacked = unpack('>L' ,data)
             self.Uptime=unpacked[0]
         else :
            print("RetTime: Unknown Subtype")
@@ -49,9 +49,9 @@ class RetTime (DCOL.Dcol) :
 
         if Dump_Level >= Dump_Summary :
             if self.Subtype==0:
-                print((" Week : {}  Time: {}".format(self.Week_Number, self.Time)))
+                print((" Week : {}  Time: {} UTC Offset: {}  ".format(self.Week_Number, self.Time, self.UTC_Offset)))
                 if Dump_Level >= Dump_Verbose :
-                    print((" UTC Offset: {}  Time Offset: {}  Time Mode".format(self.UTC_Offset, self.Time_Offset,self.Time_Mode)))
+                    print((" Time Offset: {}  Time Mode: {}".format(self.Time_Offset,self.Time_Mode)))
             else:
                 print((" Uptime : {} ".format(self.Uptime)))
 
