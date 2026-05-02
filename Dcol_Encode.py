634598e702031ca1e0f0ed566d575c71bb001fa1
jcmb <jcmb@users.noreply.github.com>
2026-05-02 10:38:51 -0600

    Update Dcol_Encode.py

--- Dcol_Encode.py
+++ Dcol_Encode.py
@@ -1,44 +1,58 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
 
 # DCOL_Encoder
 # A simple class for encoding DCOL packet.
 
 from DCOL_Decls import *
 from binascii import hexlify, unhexlify
+import sys
+from time import sleep
+from pprint import pprint
 
-class Dcol_Encode:
-    def encode (self, packet_Id, data) :
-        packet=bytearray()
-#        print "packet start"
-#        print hexlify(packet)
+class DCOL_Encode:
+    def encode(self, packet_Id: int, data):
+        packet = bytearray()
+        #        print "packet start"
+        #        print hexlify(packet)
         packet.append(STX)
-#        print "packet stx"
-#        print hexlify(packet)
-        packet.append(0) # Status Byte
+        #        print "packet stx"
+        #        print hexlify(packet)
+        packet.append(0)  # Status Byte
         packet.append(packet_Id)
         packet.append(len(data))
 
         if len(data):
-            for b in data :
+            for b in data:
                 packet.append(b)
-#                print "packet",
-#                print hexlify(packet)
+        #                print "packet",
+        #                print hexlify(packet)
 
-        Checksum = 0;
-        for i  in range (TrimComm_First_Checksum_Location,len(packet)):
-             Checksum += packet[i];
+        Checksum = 0
+        for i in range(TrimComm_First_Checksum_Location, len(packet)):
+            Checksum += packet[i]
 
         Checksum = Checksum % 256
         packet.append(Checksum)
         packet.append(ETX)
-#        print "packet"
-#        print hexlify(packet)
-
+        #        print "packet"
+        sys.stderr.buffer.write(hexlify(packet))
         return packet
 
-if __name__ == "__main__":
-   import sys
-   Encode=Dcol_Encode()
 
-#   print hexlify(Encode.encode(int(sys.argv[1],16),sys.argv[2]))
-   print((Encode.encode(int(sys.argv[1],16),unhexlify(sys.argv[2]))))
+if __name__ == "__main__":
+    import sys
+
+    Encode = DCOL_Encode()
+
+    #   print hexlify(Encode.encode(int(sys.argv[1],16),sys.argv[2]))
+    if len(sys.argv) == 3:
+        hex_string=sys.argv[2]
+        hex_string=hex_string.replace(" ", "")
+#        pprint(hex_string)
+        hex_bytes=unhexlify(hex_string)
+#        print(hex_bytes)
+        sys.stdout.buffer.write(Encode.encode(int(sys.argv[1], 16), hex_bytes))
+    else:
+        sys.stdout.buffer.write(Encode.encode(int(sys.argv[1], 16), b''))
+    sleep(5)
